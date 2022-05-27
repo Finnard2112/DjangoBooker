2ed949e803b15308b5215a4fc788792d6e603c51
Finnard2112 <73263802+Finnard2112@users.noreply.github.com>
2022-05-27 04:11:51 -0400

    Add files via upload

---
+++ bookr/reviews/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .models import Book, Review
+from .utils import average_rating
+from django.shortcuts import get_object_or_404
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({"book": book, 
+                        "book_rating": book_rating, 
+                        "number_of_reviews": number_of_reviews })
+        
+    context = {
+        'book_list': book_list
+    }
+    return render(request, 'reviews/books_list.html', context)
+
+def book_details(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+    else:
+        book_rating = None
+    context = {
+        "reviews" : reviews,
+        "book" : book, 
+        "book_rating": book_rating
+    }
+    return render(request, 'reviews/books_details.html', context)
